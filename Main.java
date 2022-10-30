366994607cbc19c7e5c0de65271d97dd04e65b8a
vladyslavkundyk <113207021+vladyslavkundyk@users.noreply.github.com>
2022-10-30 14:39:26 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -51,14 +51,13 @@ public class Main {
 		thread2.start();
 		thread3.start();
 		thread4.start();
-		
+
 		try {
 			thread1.join();
 			thread2.join();
 			thread3.join();
 			thread4.join();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
